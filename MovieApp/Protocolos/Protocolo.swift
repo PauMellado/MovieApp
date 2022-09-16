341b811ad65c7db95ba0c336699195d8be5ef67d
PauMellado <paulina.mellado.mateos@gmail.com>
2022-09-16 15:18:29 -0500

    Quitamos Lineas del Protocolo

--- MovieApp/Protocolos/Protocolo.swift
+++ MovieApp/Protocolos/Protocolo.swift
@@ -1,10 +1,3 @@
-//
-//  Protocolo.swift
-//  MovieApp
-//
-//  Created by Paulina Mellado Mateos on 11/09/22.
-//
-
 import Foundation
 protocol NotificationAlert {
     func didSelectString(_ string : String)
